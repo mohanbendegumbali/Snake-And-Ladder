97db738f2586d8fd9cc4f8d88cc870ead69c069b
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-28 19:46:05 +0530

    [Mohan] Add: Folder Structure Created

---
+++ SnakeLadderProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadderProject
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Snake and Ladder ");
+            Game newgame = new Game();
+        }
+    }
+}
